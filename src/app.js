865cd71c4cd37224944539dbd65b4f8a3924f6c9
Thelgaris <alex.gabarron84@gmail.com>
2022-03-15 19:27:12 +0000

    firstone

--- src/app.js
+++ src/app.js
@@ -1,11 +1,14 @@
-/* eslint-disable */
-import "bootstrap";
-import "./style.css";
+let pronoun = ["the", "our"];
+let adj = ["great", "big"];
+let noun = ["jogger", "racoon"];
+let combinations = "";
+let extension = ".com";
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
-
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
-};
+for (let i = 0; i < pronoun.length; i++) {
+  for (let j = 0; j < adj.length; j++) {
+    for (let k = 0; k < noun.length; k++) {
+      combinations = pronoun[i] + adj[j] + noun[k] + extension;
+      console.log(combinations);
+    }
+  }
+}
